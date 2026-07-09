b7fcf43277366ed08ed0ca5f5cefe22c846f7a9e
Sarbo264 <sahasarbajit2007@gmail.com>
2026-07-10 00:22:42 +0530

    Making the Logic of Finding the Article Numbers more Stable

--- Program.py
+++ Program.py
@@ -1,51 +1,54 @@
-# Importing PdfReader from the Library PyPDF2
-from PyPDF2 import PdfReader
+# Importing articles_list and Omitted_articles from the ArticleList file
+from ArticleList import articles_list, Omitted_articles
+# Importing full_article from Text file
+from Text import full_article
+# Declairing a main function.
 def main():
     # Taking a Variable which will take input of the Article Number from the User.
     keyword = input("Enter the Article Number you want to find the Description: ")
+    # Storing the extracted text in the output.
     output = extraction(keyword)
+    # Printing the Output.
     print(output)
-    
-# Makking a function to manage this operations.
+
+# Declairing a extraction function whose parameters will be keywords.
 def extraction (keyword):
-    # Defining a Variable for reading the Pdf
-    reader = PdfReader("Constituition.pdf")
-    # Checking if the given Input is a integer or not.
-    if not keyword.isdigit():
-        return 'Invalid Input.\nPlease Try Again.\nPlease note: This Program does not accept any inputs other than Integer.'
-    # Excluding Article No. 238 since it is ommited in the constituition and it does not exist.
-    elif keyword == '238':
-        return 'The Article Number is Ommited in the Constituition of India.\nPlease Try Again.'
-    # Excluding Negative numbers and those numbers which are greater than 395 since 395 is the last Article number.
-    elif int(keyword) < 1 or int(keyword) > 395:
-        return 'Invalid Article Number.\n Please Try Again.'  
-    # Working with those Articles which does not satisfy those above conditions.
-    else:
-        # Declaring an empty String.
-        text = ''
-        # Declaring a for loop to iterate through the pages of the Pdf and extract the text from each page and store it in the text variable.
-        for i in range(len(reader.pages)):
-            # Extracting the text from each page and storing it in the text variable.
-            text = text + reader.pages[i].extract_text()
-        # Using try for handling Formating error and handling unexpected crashesh.
+    # Extracting all the text of the PDF in the text variable.
+    text = full_article()
+    # Checking if the keyword is in the articles_list or not.
+    if keyword in articles_list:
         try:
-            # Declaring a variable to find the index of the keyword in the text variable.
-            index1 = text.index(keyword + '.')
-            # Making logics for the Article Numbers apart from the Last Article number which is 395.
-            if keyword != '395':
-                # Declaring a variable to find the index of the next keyword in the text variable.
-                index2 = text.index(str(int(keyword)+1)+ '.')
-                # Returning the text from the index1 to index2 which will give the description of the Article Number entered by the User.
+            # Finding the index number of the keyword in the articles_list
+            article_index = articles_list.index(keyword)
+            # Finding the next article number.
+            next_article = articles_list[article_index + 1]
+            # Finding the index number of the required article number in the text variable.
+            index1 = text.index(keyword+'.')
+            # Checking if the article number is not the last Article number in the Constitution.
+            if keyword != articles_list[len(articles_list)-1]:
+                # Finding the index number of the next_article in the text variable.
+                index2 = text.index(next_article+'.',index1 + len(keyword))
+                # Checking for Duplication in the Text Variable
+                duplicate = text.find(keyword+'.',index1+len(keyword),index2)
+                # Checking if the any duplication is found or not.
+                if duplicate != -1:
+                    # Making the new value of index2 to the duplicate value.
+                    index2 = duplicate
+                # Returning the text from thr index1 to index2
                 return text[index1:index2]
-            # Making logic for the Article number 395.
             else:
-                # Returnung text from index1 to end of the file.
+                # Returning the text from the index1 to the end of the text variable.
                 return text[index1:]
         except:
-            return f'Formatting error: Could not locate the {keyword} in the PDF file.'
-    # Adding a function for reading .md Files.
-def read_markdown_file(markdown_file):
-    with open(markdown_file, 'r', encoding = 'utf-8') as file:
-        return file.read()
+            return f'Formatting Error in the PDF.\n{keyword} not found in the PDF.\nPlease Try Again.'
+    # Checking if the keyword is in the Omitted_articles or not.
+    elif keyword in Omitted_articles:
+        # Returning the Error Message for Ommited Articles.
+        return 'The Article Number is Ommited in the Constituition of India.\nPlease Try Again.'
+    # If the keyword is not in the articles_list or Omitted_articles then returning the Error Message.
+    else:
+        # Returning the Error Message for Invalid Articles.
+        return 'Invalid Article Number.\n Please Try Again.'
+# Calling the main function.
 if __name__ == '__main__':
     main()
